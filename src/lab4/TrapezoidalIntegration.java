a7ca7478b1493dccf408250009a29a37532999b3
7Kseniya <ksenia7ignatyeva@gmail.com>
2024-04-22 14:48:51 +0300

    lab4 is done

--- src/lab4/TrapezoidalIntegration.java
+++ src/lab4/TrapezoidalIntegration.java
@@ -1,12 +1,12 @@
 package lab4;
+
 import static java.lang.StrictMath.*;
 
-import java.util.function.Function;
 
 class TrapezoidalIntegration {
     public static String error_message = "";
     public static boolean has_discontinuity = false;
-    
+
     private static double first_function(double x) {
         return 1 / x;
     }
@@ -28,10 +28,10 @@ class TrapezoidalIntegration {
     }
 
     /*
-    * How to use this function:
-    *    Function<Double, Double> func = get_function(4);
-    *    func.apply(0.0001);
-    */
+     * How to use this function:
+     * Function<Double, Double> function = get_function(4);
+     * function.apply(0.0001);
+     */
     private static Function<Double, Double> get_function(int n) {
         switch (n) {
             case (1):
@@ -48,53 +48,44 @@ class TrapezoidalIntegration {
                 throw new UnsupportedOperationException("Function " + n + " not defined.");
         }
     }
-    
-    
+
     /*
      * Complete the 'calculate_integral' function below.
      *
      * The function is expected to return a DOUBLE.
      * The function accepts following parameters:
-     *  1. DOUBLE a
-     *  2. DOUBLE b
-     *  3. INTEGER f
-     *  4. DOUBLE epsilon
+     * 1. DOUBLE a
+     * 2. DOUBLE b
+     * 3. INTEGER f
+     * 4. DOUBLE epsilon
      */
 
     public static double calculate_integral(double a, double b, int f, double epsilon) {
-        if(a > b) {
-            double temp = a;
-            a = b;
-            b = temp;
-        }
         Function<Double, Double> function = get_function(f);
+        double h = (b - a);
+        double integral = (function.apply(a) + function.apply(b)) / 2.0;
 
-        double h = b - a;
-        double integral = 0.5 * h * (function.apply(a) + function.apply(b));
-        double prevIntegral = 0.0;
-
-
-        while(abs(integral - prevIntegral) > epsilon) {
-            prevIntegral = integral;
-            h = h / 2;
-            integral = 0.5 * prevIntegral + h * sum(function, a, b, h);
-        }
-
-        return integral;
-
-    }
-    private static double sum(Function<Double, Double> func, double a, double b, double h) {
-        double sum = 0.0;
-        for(double x = a + h; x < b; x += h) {
-            if(Double.isNaN(func.apply(x)) || Double.isInfinite(func.apply(x))) {
-                error_message = "integrated function has discontinuity or does not defined in current interval";
-                has_discontinuity = true;
-                return 0.0;
+        double newIntegral;
+        do {
+            newIntegral = 0;
+            h /= 2.0;
+            for (double x = a + h; x < b; x += h) {
+                if (Double.isNaN(function.apply(x)) || Double.isInfinite(function.apply(x))) {
+                    error_message = "Integrated function has discontinuity or does not defined in current interval";
+                    has_discontinuity = true;
+                    return 0.0;
+                }
+                newIntegral += function.apply(x);
             }
-            sum += func.apply(x);
-        }
-        return sum;
+            newIntegral *= h;
+            if (Math.abs(integral - newIntegral) < epsilon) {
+                integral = newIntegral;
+                break;
+            } else {
+                integral = newIntegral;
+            }
+        } while (true);
+
+        return a > b ? -integral : integral;
     }
-
 }
-
